a216605f3d3549aa2c584d43e30d4a4023820c22
Beanie <beaniemcc1@gmail.com>
2023-04-30 15:19:15 +1000

    stuff on pi

--- main.py
+++ main.py
@@ -3,33 +3,21 @@
 # method of detecting which pin has been clicked
 # doing something about it
 # map rects to a dictionary
-
+black = 0, 0, 0
 import gpiozero # pi GPIO
-import pygame
-
-# pygame setup
+import pygame, sys
 pygame.init()
-pygame.image.load("EEPROM.png")
 screen = pygame.display.set_mode((600, 600))
-clock = pygame.time.Clock()
-running = True
 pygame.display.set_caption("EEPROM viewer")
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+EEPROM = pygame.image.load("EEPROM.png")
+EEPROMrect = EEPROM.get_rect()
+
+while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        if event.type == pygame.QUIT: sys.exit()
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-
-    # RENDER YOUR GAME HERE
-
-    # flip() the display to put your work on screen
+    screen.fill(black)
+    screen.blit(EEPROM, EEPROMrect)
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
-
-pygame.quit()
