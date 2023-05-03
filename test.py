ce002c3eab7b3856bea2267d49f7746bb4e25d08
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2023-05-04 08:48:25 +1000

    lots of stuff

--- test.py
+++ test.py
@@ -1,10 +1,20 @@
-from gpiozero import LED
-from time import sleep
+code = bytearray([
+  0xa9, 0xff,         # lda #$ff
+  0x8d, 0x02, 0x60,   # sta $6002
 
-led = LED(17)
+  0xa9, 0x55,         # lda #$55
+  0x8d, 0x00, 0x60,   # sta $6000
 
-while True:
-    led.on()
-    sleep(1)
-    led.off()
-    sleep(1)
+  0xa9, 0xaa,         # lda #$aa
+  0x8d, 0x00, 0x60,   # sta $6000
+
+  0x4c, 0x05, 0x80,   # jmp $8005
+  ])
+
+rom = code + bytearray([0xea] * (32768 - len(code)))
+
+rom[0x7ffc] = 0x00
+rom[0x7ffd] = 0x80
+
+with open("rom.bin", "wb") as out_file:
+  out_file.write(rom)
